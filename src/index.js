f471e040231bebebab88b80d599ac09304601e20
lilianatutza <lilianatutza@gmail.com>
2024-05-21 22:28:34 +0100

    add another variable to JS and round the temperature
    now we store the temperature in a temperature variable in the function
    and we say  the temperature to be equal to math. Round

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature")
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round;
   console.log(response.data.temperature.current);
 }
 
 function searchCity(city) {
-let apiKey = "bb604a31635tbbcod35f93428afe08ae";
-let apiURL =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&untis=metric`;
-  axios.get(apiURL).then(refreshWeather)
-  
+  let apiKey = "bb604a31635tbbcod35f93428afe08ae";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&untis=metric`;
+  axios.get(apiURL).then(refreshWeather);
 }
 
-
-
 function handleSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
-searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
