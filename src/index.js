d931d069ea12ebd712117daf53b2563be800499c
lilianatutza <lilianatutza@gmail.com>
2024-05-21 23:14:10 +0100

    worked on humidity
    1, we work on humidity now
    2 console log date and notice we have the humidity level inside temperature
    3. in html we remove the 66% and we add an id in the strong element
     and give it a name if humidity .
    4 go to JS and create a new element
    5.then write this code
    humidityElement.innerHTML= response.data.temperature.humidity;
    6. to fix the percentage do this
      humidityElement.innerHTML= `${response.data.temperature.humidity}%`;

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
 
   console.log(response.data.condition.description);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
-temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
