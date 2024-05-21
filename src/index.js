39cf7a1e61b7b5440efc59bbec259af732d8e169
lilianatutza <lilianatutza@gmail.com>
2024-05-21 21:36:49 +0100

    Update index.js
    Once we have this city that is being searched we call it; we do this by adding this line of code : searchCity(searchInput.value); in the city function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
