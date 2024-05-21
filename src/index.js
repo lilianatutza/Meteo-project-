9f0e0ec6736adbe84f3bd415beed90842eaef09b
lilianatutza <lilianatutza@gmail.com>
2024-05-21 21:08:49 +0100

    added and id to html and a new variable to target this id in JS
    Now we want to target the name which we know is the h1 in js;  so we go to the html and ad an id so we can then target it the JS.
    
    we call the id"city"
    
    in JS we call the city element( OR city variable) is the result of the city element; we use the # sign because we target an id. if we would target a class we would use a dot.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-console.log(searchInput.value)
-}
+let cityElement = document.querySelector("#city");
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
