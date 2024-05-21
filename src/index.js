8ce41cd2c6b5f484e08995b238984800b55badf2
lilianatutza <lilianatutza@gmail.com>
2024-05-22 00:10:01 +0100

    work on the time in HTML and JS
    use API
    1. Go to html and add and create and new id and span around the day. Name the id "time". Remove the hardcoding of day and time
    2 ADD the variable in JS the same as you've done for humidity
    3.parse timestamp using  new Date * 1000
    create a new function to format the date

--- src/index.js
+++ src/index.js
@@ -5,14 +5,35 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-  console.log(response.data.condition.description);
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedensday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
