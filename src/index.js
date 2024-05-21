4fe899ddf886c6eabd81bcb2db1f96b618d85e05
lilianatutza <lilianatutza@gmail.com>
2024-05-21 20:56:12 +0100

    add a new variable and conslole.lotgit
    create a new variable called let searchInput listen to an event query selector that is going to be # and the name if the id that we just created in HTML.
    
    console log console.log(searchInput.value). type a name in the search feeld and it should be visble in the console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-function namehandleSearchSubmit(event) {
-event.preventDefault()
+function handleSearchSubmit(event) {
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+console.log(searchInput.value)
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
