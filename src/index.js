0d8ef0ad7ec3e017496f272970205ff09162c658
lilianatutza <lilianatutza@gmail.com>
2024-05-21 20:43:12 +0100

    add event to prevent default behaviour
    The default behaviour is reloading the page or going to the action of the form
    
    so we add an event to prevent this-

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function namehandleSearchSubmit(event) {
-  
+event.preventDefault()
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
