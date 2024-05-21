8828b0a1d0f0d1722523b29002571c2ea740d4ff
lilianatutza <lilianatutza@gmail.com>
2024-05-21 21:21:03 +0100

    we add the inner html in side the value
    we test by typing a name in the search space and we test it by typing city in the search space

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
-cityElement = searchInput.value;
+cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
